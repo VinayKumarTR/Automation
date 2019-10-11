20e75fd20b1527943a13c2de6905ff5b26e75a70
VinayKumarTR <vinaykumartr@yahoo.com>
2019-10-11 09:48:02 +0530

    HomePage Object & Test

---
+++ src/main/java/com/Application/myApplication/HomePageObjects.java
@@ -0,0 +1,111 @@
+package com.Application.myApplication;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageObjects extends BasePage {
+	@FindBy(xpath="//*[@class='sf-with-ul' and @title='Women']")
+	WebElement Women;
+	
+	@FindBy(xpath="//*[@id='block_top_menu']/ul/li[2]/a")
+	WebElement Dresses;
+	
+	@FindBy(xpath="//*[@id='block_top_menu']/ul/li[3]/a")
+	WebElement TShirts;
+	
+	@FindBy(xpath="//*[@id='newsletter-input']")
+	WebElement NewsLetter;
+	
+	@FindBy(xpath="//*[@id='newsletter_block_left']/div/form/div/button")
+	WebElement NewsLetterButton;
+	
+	@FindBy(xpath="//*[@id='columns']/p")
+	WebElement Subscription;
+	
+	@FindBy(xpath="//*[@id='block_top_menu']/ul/li[2]/a")
+	WebElement Select_Dresses;
+	
+	@FindBy(xpath="//*[@id='ul_layered_id_attribute_group_1']/li[1]/label/a")
+	WebElement S_Size;
+	
+	@FindBy(xpath="//*[@id='ul_layered_id_attribute_group_1']/li[2]/label/a")
+	WebElement M_Size;
+	
+	@FindBy(xpath="//*[@id='ul_layered_id_attribute_group_1']/li[3]/label/a")
+	WebElement L_Size;
+	
+	@FindBy(xpath="//*[@id='center_column']/ul/li[1]/div/div[1]/div/a[1]/img")
+	WebElement OrangeDress;
+	
+	@FindBy(xpath="//*[@id='product']/div/div/div[2]/p[7]/button[1]")
+	WebElement Tweet;
+	
+	@FindBy(xpath="//*[@id='product']/div/div/div[2]/p[7]/button[2]")
+	WebElement Share;
+	
+	@FindBy(xpath="//*[@id='add_to_cart']/button/span")
+	WebElement AddToCart;
+	
+	public HomePageObjects(){
+	
+	  	PageFactory.initElements(driver, this);
+	}
+	
+	public WebElement WomenfromSite(){
+		return Women;
+	}
+	
+	public WebElement DressesfromSite(){
+		return Dresses;
+	}
+	
+	public WebElement TShirtsfromSite(){
+		return TShirts;
+	}
+	
+	public WebElement DataInNewsLetter(){
+		return NewsLetter;
+	}
+	
+	public WebElement NewsLetterClick(){
+		return NewsLetterButton;
+	}
+	
+	public WebElement SubscriptionMessage(){
+		return Subscription;
+	}
+	
+	public WebElement selectionofdresses(){
+		return Select_Dresses;
+	}
+	
+	public WebElement SelectSizeS(){
+		return S_Size;
+	}
+	
+	public WebElement SelectSizeM(){
+		return M_Size;
+	}
+	public WebElement SelectSizeL(){
+		return L_Size;
+	}
+	public WebElement SelectionOfOrangeDress1(){
+		return OrangeDress;
+	}
+	
+	public WebElement Tweeting(){
+		return Tweet;
+	}
+	
+	public WebElement Sharing(){
+		return Share;
+	}
+	
+	public WebElement AddingToCart(){
+		return AddToCart;
+	}
+	
+	
+}
---
+++ src/test/java/com/Application/myApplication/HomePageTests.java
@@ -0,0 +1,75 @@
+package com.Application.myApplication;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTests {
+	HomePageObjects HP;
+	BasePage BP;
+	
+	public HomePageTests(){
+		HP=new HomePageObjects();
+		BP=new BasePage();
+	}
+		
+		@Test(priority=1)
+		public void verifyAllText(){
+			Assert.assertTrue(HP.WomenfromSite().isDisplayed());
+			Assert.assertTrue(HP.DressesfromSite().isDisplayed());
+			Assert.assertTrue(HP.TShirtsfromSite().isDisplayed());
+		}
+//		@Test(priority=2)
+//		public void verifyDressesText(){
+//			Assert.assertTrue(HP.DressesfromSite().isDisplayed());
+//		}
+//		@Test(priority=3)
+//		public void verifyTShirtText(){
+//			Assert.assertTrue(HP.TShirtsfromSite().isDisplayed());
+//		}
+		
+		@Test(priority=2)
+		public void VerifyNewsLetterData(){
+			HP.DataInNewsLetter().sendKeys("vinayTR@gmail.com");
+		}
+		
+		@Test(priority=3)
+		public void verifyNewsLetterButton(){
+			HP.NewsLetterClick().click();
+		}
+		
+		@Test(priority=4)
+		public void VerifySubscription(){
+			HP.SubscriptionMessage().isDisplayed();
+		}
+		
+		@Test(priority=5)
+		public void verifyselectionofdresses(){
+			HP.selectionofdresses().click();
+		}
+		
+		@Test(priority=6)
+		public void verifyingSize(){
+			HP.SelectSizeS().isDisplayed();
+			HP.SelectSizeM().isDisplayed();
+			HP.SelectSizeL().isDisplayed();
+		}
+
+		@Test(priority=7)
+		public void verifyOrangeDress(){
+			HP.SelectionOfOrangeDress1().click();
+		}	
+		
+		@Test(priority=7)
+		public void verifyTwitter(){
+			Assert.assertTrue(HP.Tweeting().isDisplayed());
+		}
+		@Test(priority=8)
+		public void verifyShare(){
+			Assert.assertTrue(HP.Sharing().isDisplayed());	
+		}
+		
+		@Test(priority=9)
+		public void verifyAddingToCart(){
+			HP.AddingToCart().click();
+		}
+}
